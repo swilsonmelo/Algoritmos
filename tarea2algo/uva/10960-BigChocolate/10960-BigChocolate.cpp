b836aff16f0c30388640534acce93cf4195d3181
swilsonmelo <swilsonmelo@gmail.com>
2019-01-31 22:03:27 -0500

    tarea 2 algo

---
+++ tarea2algo/uva/10960-BigChocolate/10960-BigChocolate.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int n,m;
+    while(scanf("%d %d",&n,&m) == 2){
+        printf("%d\n",n*m - 1);
+    }
+
+    return 0;
+}
